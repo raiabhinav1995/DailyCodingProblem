b81b03df7ff6a8bf1d9d7488c70b22fa76f9eca8
Abhinav <raiabhinav1995@gmail.com>
2020-04-23 00:43:50 +0530

    Made Edureka first program
    Made Edureka first program

---
+++ lauralehsan/lauralehsan.js
@@ -0,0 +1,10 @@
+let programmingLanguages=[];
+programmingLanguages.push("C++");
+programmingLanguages.push("Java");
+programmingLanguages.push("Python");
+programmingLanguages.push("C#.Net");
+function alertToConsole(programmingLanguages)
+{
+    alert(programmingLanguages);
+}
+alertToConsole(programmingLanguages);
